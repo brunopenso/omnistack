d50079275126c60cadcc28720041aff2c945ac8c
brunopenso <viperp@gmail.com>
2019-10-04 06:52:46 -0300

    new route

--- backend/src/controller/SessionController.js
+++ backend/src/controller/SessionController.js
@@ -9,9 +9,11 @@ module.exports = {
         //attribute with the same name
         //const email = req.body.email;
         const { email } = req.body;
-        
-        const user = await User.create({ email });
-        
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email });
+        }
         return res.json(user);
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const routes = express.Router();
 
 const SessionController = require('./controller/SessionController');
+const SpotController = require('./controller/SpotController');
 
 routes.post('/sessions', SessionController.store);
+routes.post('/spots', SpotController.store);
 
 module.exports = routes;
