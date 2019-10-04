8675a670ed74c0061da875a7675c967e3f1652b1
brunopenso <viperp@gmail.com>
2019-10-04 06:42:29 -0300

    fix dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const routes = require('./routes')
 const mongoose = require('mongoose');
 const app = express();
+const mongoUrl = "mongodb+srv://omnistack:omnistack@omnistack-oi2ie.mongodb.net/mydb?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb+srv://omnistack:omnistack@omnistack-oi2ie.mongodb.net/mydb?retryWrites=true&w=majority",
+mongoose.connect(mongoUrl,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
-).catch( error => console.log(error))
+)
+.catch( error => console.log(error))
 .then(console.log('Mongoose is connected'))
 .finally(console.log('Mongoose connect method end'));
 
@@ -18,9 +20,6 @@ mongoose.connect("mongodb+srv://omnistack:omnistack@omnistack-oi2ie.mongodb.net/
 
 //tells to express to understand json body messages
 app.use(express.json());
-
+//use the routes files
 app.use(routes);
-
-//mongodb+srv://omnistack:<password>@omnistack-oi2ie.mongodb.net/admin?retryWrites=true&w=majority
-
 app.listen(3333);
