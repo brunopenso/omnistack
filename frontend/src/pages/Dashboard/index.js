e141a80e7b0e27e5afd101955eb8d373c8c768b9
brunopenso <viperp@gmail.com>
2019-10-05 11:48:17 -0300

    list of spots

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
 export default function Dashboard() {
-    return (<div/>);
+    const [spots, setSpots] = useState([]);
+    //if the last parameter arrays is empty it tells that the function will be executed only one time
+    useEffect( () => {
+        async function loadSpots() {
+            const userid = localStorage.getItem('user');
+            const response = await api.get("/dashboard", {
+                headers: {userid}
+            });
+
+            setSpots(response.data);
+        }
+
+        loadSpots();
+    },[]);
+
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
