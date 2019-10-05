2ccbffdc13a46c7e50fc77ee7b4eb330875dfc29
brunopenso <viperp@gmail.com>
2019-10-05 11:02:11 -0300

    css for login done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
-
+import logo from './assets/logo.svg';
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+      <img src={logo} alt="Air CnC"/>
+      <div className="content">
+        <p>Ofereça <strong>Spots</strong> para programadores e encontre <strong>talentos</strong></p>
+
+        <form>
+          <label htmlFor="email">Email *</label>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="Seu melhor e-mail"/>
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+
+      </div>
+    </div>
   );
 }
 
